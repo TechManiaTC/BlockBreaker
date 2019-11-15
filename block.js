e45e63119a3174a9fcea408224c6bd2f4c09a896
chentao <tech.chen@outlook.com>
2019-11-15 11:56:55 +0800

    change tabSize 4

--- block.js
+++ block.js
@@ -1,38 +1,38 @@
-var Block = function(position) {
-  // position是坐标 [0, 0]
-  var image = imageFromPath('block.png')
-  var o = {
-    image: image,
-    x: position[0],
-    y: position[1],
-    alive: true,
-    health: position[2] || 1,
-  }
-  o.kill = function() {
-    o.health -= 1
-    if (o.health < 1) {
-      o.alive = false
+var Block = function (position) {
+    // position是坐标 [0, 0]
+    var image = imageFromPath('block.png')
+    var o = {
+        image: image,
+        x: position[0],
+        y: position[1],
+        alive: true,
+        health: position[2] || 1,
     }
-  }
-  o.intersectRect = function(o1, o2) {
-    var ax1 = o1.x
-    var ax2 = o1.x + o1.image.width
-    var ay1 = o1.y
-    var ay2 = o1.y + o1.image.height
+    o.kill = function () {
+        o.health -= 1
+        if (o.health < 1) {
+            o.alive = false
+        }
+    }
+    o.intersectRect = function (o1, o2) {
+        var ax1 = o1.x
+        var ax2 = o1.x + o1.image.width
+        var ay1 = o1.y
+        var ay2 = o1.y + o1.image.height
 
-    var bx1 = o2.x
-    var bx2 = o2.x + o2.image.width
-    var by1 = o2.y
-    var by2 = o2.y + o2.image.height
-    // log(ax1,ax2,ay1,ay2,bx1,bx2,by1,by2)
-    return ((ax1 <= bx2) && (ax2 >= bx1) && (ay1 <= by2) && (ay2 >= by1))
-  }
-  o.collide = function(ball) {
-    if (o.alive && o.intersectRect(ball, o)) {
-      log('block 相撞')
-      return true
+        var bx1 = o2.x
+        var bx2 = o2.x + o2.image.width
+        var by1 = o2.y
+        var by2 = o2.y + o2.image.height
+        // log(ax1,ax2,ay1,ay2,bx1,bx2,by1,by2)
+        return ((ax1 <= bx2) && (ax2 >= bx1) && (ay1 <= by2) && (ay2 >= by1))
     }
-    return false
-  }
-  return o
+    o.collide = function (ball) {
+        if (o.alive && o.intersectRect(ball, o)) {
+            log('block 相撞')
+            return true
+        }
+        return false
+    }
+    return o
 }
--- engine.js
+++ engine.js
@@ -1,52 +1,52 @@
-var Engine = function() {
-  var g = {
-    actions: {},
-    keydowns: {},
-  }
-  var canvas = document.querySelector('#id-canvas')
-  var context = canvas.getContext('2d')
-  g.canvas = canvas
-  g.context = context
-  // draw
-  g.drawImage = function(image) {
-    g.context.drawImage(image.image, image.x, image.y)
-  }
-  // events
-  window.addEventListener('keydown', function(event) {
-    g.keydowns[event.key] = true
-  })
-  window.addEventListener('keyup', function(event) {
-    g.keydowns[event.key] = false
-  })
-  // registerAction
-  g.registerAction = function(key, callback) {
-    g.actions[key] = callback
-  }
-
-  window.fps = 30
-  var runloop = function () {
-    var actions = Object.keys(g.actions)
-    // log('g.actions', Object.keys(g))
-    for (var i = 0; i < actions.length; i++) {
-      var key = actions[i]
-      if  (g.keydowns[key]) {
-        // if key pressed, call register action
-        g.actions[key]()
-      }
+var Engine = function () {
+    var g = {
+        actions: {},
+        keydowns: {},
     }
-    // update status
-    g.update()
-    // clear
-    context.clearRect(0, 0, canvas.width, canvas.height)
+    var canvas = document.querySelector('#id-canvas')
+    var context = canvas.getContext('2d')
+    g.canvas = canvas
+    g.context = context
     // draw
-    g.draw()
-    setTimeout(function() {
-      runloop()
-    }, 1000/window.fps)
-  }
+    g.drawImage = function (image) {
+        g.context.drawImage(image.image, image.x, image.y)
+    }
+    // events
+    window.addEventListener('keydown', function (event) {
+        g.keydowns[event.key] = true
+    })
+    window.addEventListener('keyup', function (event) {
+        g.keydowns[event.key] = false
+    })
+    // registerAction
+    g.registerAction = function (key, callback) {
+        g.actions[key] = callback
+    }
 
-  setTimeout(function() {
-    runloop()
-  }, 1000/window.fps)
-  return g
+    window.fps = 30
+    var runloop = function () {
+        var actions = Object.keys(g.actions)
+        // log('g.actions', Object.keys(g))
+        for (var i = 0; i < actions.length; i++) {
+            var key = actions[i]
+            if (g.keydowns[key]) {
+                // if key pressed, call register action
+                g.actions[key]()
+            }
+        }
+        // update status
+        g.update()
+        // clear
+        context.clearRect(0, 0, canvas.width, canvas.height)
+        // draw
+        g.draw()
+        setTimeout(function () {
+            runloop()
+        }, 1000 / window.fps)
+    }
+
+    setTimeout(function () {
+        runloop()
+    }, 1000 / window.fps)
+    return g
 }
