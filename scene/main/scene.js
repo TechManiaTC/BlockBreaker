217513bdaf75fc4f5d0b2864ac839e4cfa1c70e3
chentao <tech.chen@outlook.com>
2019-11-27 22:39:22 +0800

    refactor

--- scene/main/scene.js
+++ scene/main/scene.js
@@ -1,74 +1,80 @@
-var Scene = function(game) {
-    var s = {
-        game: game
+class Scene extends AbstractScene {
+    constructor(game) {
+        super(game)
+        this.paddle = Paddle(game)
+        this.ball = Ball(game)
+        this.score = 0
+        this.blocks = loadLevel(game, 1)
+        this.enableDrag = false
+        game.registerAction('r', function() {
+            var start = new SceneTitle(this.game)
+            this.game.replaceScene(start)
+        })
+        this.move(this.paddle, this.ball)
+        this.mouse(this.ball, this.enableDrag)
     }
-    var paddle = Paddle(game)
-    var ball = Ball(game)
-    var score = 0
-    blocks = loadLevel(game, 1)  
-
-    s.draw = function() {
-        game.drawImage(paddle)
-            game.drawImage(ball)
-            for (var i = 0; i < blocks.length; i++) {
-                if (blocks[i].alive) {
-                    game.drawImage(blocks[i])
-                }
+    draw() {
+        this.game.drawImage(this.paddle)
+        this.game.drawImage(this.ball)
+        for (var i = 0; i < this.blocks.length; i++) {
+            if (this.blocks[i].alive) {
+                this.game.drawImage(this.blocks[i])
             }
-            game.context.fillText('分数: ' + score, 10, 290)
+        }
+        this.game.context.fillText('分数: ' + this.score, 10, 290)
     }
-    s.update = function() {
-        ball.move()
+    update() {
+        this.ball.move()
         // 游戏结束
-        if (ball.y > paddle.y) {
-            var end = new SceneEnd(game)
-            game.replaceScene(end)
+        if (this.ball.y > this.paddle.y) {
+            var end = new SceneEnd(this.game)
+            this.game.replaceScene(end)
         }
         // 板球相撞则反弹
-        if (paddle.collide(ball)) {
-            ball.rebound()
+        if (this.paddle.collide(this.ball)) {
+            this.ball.rebound()
         }
         // 每帧循环所有砖块判断是否和球相撞
-        for (var i = 0; i < blocks.length; i++) {
-            if (blocks[i].collide(ball)) {
-                blocks[i].kill()
-                ball.rebound()
+        for (var i = 0; i < this.blocks.length; i++) {
+            if (this.blocks[i].collide(this.ball)) {
+                this.blocks[i].kill()
+                this.ball.rebound()
                 // update score
-                score += 100
+                this.score += 100
             }
         }
     }
-
-    game.registerAction('a', function() {
-        paddle.moveLeft()
-    })
-    game.registerAction('d', function() {
-        paddle.moveRight()
-    })
-    game.registerAction('f', function() {
-        ball.fire()
-    })
-
+    // move
+    move(paddle, ball) {
+        this.game.registerAction('a', function() {
+            paddle.moveLeft()
+        })
+        this.game.registerAction('d', function() {
+            paddle.moveRight()
+        })
+        this.game.registerAction('f', function() {
+            ball.fire()
+        })
+    }
     // mouese event
-    var enableDrag = false
-    window.addEventListener('mousedown', function(event) {
-        var x = event.offsetX
-        var y = event.offsetY
-        if (ball.hasPoint(x, y)) {
-            enableDrag = true
-        }
-    });
-    window.addEventListener('mousemove', function(event) {
-        var x = event.offsetX
-        var y = event.offsetY
-        if (enableDrag) {
-            ball.x = x
-            ball.y = y
-        }
-    });
-    window.addEventListener('mouseup', function(event) {
-        enableDrag = false
-    });  
-
-    return s
+    mouse(ball, enableDrag) {
+        window.addEventListener('mousedown', function(event) {
+            var x = event.offsetX
+            var y = event.offsetY
+            if (ball.hasPoint(x, y)) {
+                enableDrag = true
+            }
+        });
+        window.addEventListener('mousemove', function(event) {
+            var x = event.offsetX
+            var y = event.offsetY
+            if (enableDrag) {
+                ball.x = x
+                ball.y = y
+            }
+        });
+        window.addEventListener('mouseup', function(event) {
+            enableDrag = false
+        });  
+    }
 }
